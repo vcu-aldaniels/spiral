b5a71b8b0623d41f9d6615b85a13cfdd671928ad
Alexis Daniels <danielsam3@vcu.edu>
2021-04-09 12:45:45 -0400

    change 6

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,18 +1,19 @@
 def spiralize(number):
-      
+
     n = (number - 1) / 2
-    
-    x = (3 + 2 * n * (8 * n * n + 15 * n +13)) / 3
-    
+
+    x = (3 + 2 * n * (8 * n * n + 15 * n + 13)) / 3
+
     return_value = x
 
     return return_value
-    
+
+
 def sum():
 
-  diagonal = spiralize(501)
- 
-  print (diagonal)
-  
-sum()
+    diagonal = spiralize(501)
 
+    print(diagonal)
+
+
+sum()
